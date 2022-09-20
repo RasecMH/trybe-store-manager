287e81fc7e17324d9c110f72b518028a410c1865
RasecMH <rasecmh0@gmail.com>
2022-09-20 11:27:39 -0300

    create controllers setup

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,22 @@
+const { productsService } = require('../services');
+
+const listProducts = async (req, res) => {};
+
+const listProductById = async (req, res) => {};
+
+const listProductsBySearchTerm = async (req, res) => {};
+
+const createProduct = async (req, res) => {};
+
+const updateProductById = async (req, res) => {};
+
+const deleteProductById = async (req, res) => {};
+
+module.exports = {
+  listProducts,
+  listProductById,
+  listProductsBySearchTerm,
+  createProduct,
+  updateProductById,
+  deleteProductById,
+};
---
+++ src/controllers/sales.controller.js
@@ -0,0 +1,19 @@
+const { salesService } = require('../services');
+
+const listSales = async (req, res) => {};
+
+const listSaleById = async (req, res) => {};
+
+const createSale = async (req, res) => {};
+
+const updateSaleById = async (req, res) => {};
+
+const deleteSaleById = async (req, res) => {};
+
+module.exports = {
+  listSales,
+  listSaleById,
+  createSale,
+  updateSaleById,
+  deleteSaleById,
+};
