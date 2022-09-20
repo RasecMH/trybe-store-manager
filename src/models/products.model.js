2d8eea2f4033b8937addb5d951ef89c258223ece
RasecMH <rasecmh0@gmail.com>
2022-09-20 11:58:09 -0300

    create models setup

---
+++ src/models/products.model.js
@@ -0,0 +1,24 @@
+const camelize = require('camelize');
+const snakeize = require('snakeize');
+const connection = require('./connection');
+
+const insert = async (product) => {};
+
+const getAll = async () => {};
+
+const findById = async (productId) => {};
+
+const findByName = async (productName) => {};
+
+const updateById = async (productId) => {};
+
+const deleteById = async (productId) => {};
+
+module.exports = {
+  insert,
+  getAll,
+  findById,
+  findByName,
+  updateById,
+  deleteById,
+};
---
+++ src/models/sales.model.js
@@ -0,0 +1,21 @@
+const camelize = require('camelize');
+const snakeize = require('snakeize');
+const connection = require('./connection');
+
+const insert = async (sale) => {};
+
+const getAll = async () => {};
+
+const findById = async (saleId) => {};
+
+const updateById = async (saleId) => {};
+
+const deleteById = async (saleId) => {};
+
+module.exports = {
+  insert,
+  getAll,
+  findById,
+  updateById,
+  deleteById,
+};
